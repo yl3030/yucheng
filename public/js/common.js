2c0a3433f602053aea810c79b31b8c940b5a67d4
youzhen <yl301114@gmail.com>
2022-09-08 00:18:43 +0800

    剩代理

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,11 @@
+AOS.init();
+
 // menu
 $(".header_menu").click(function(){
     $(".header_nav").slideToggle(300);
+})
+
+// top
+$(".btn-top").click(function(){
+    $("html,body").animate({ scrollTop:0 },300)
 })
