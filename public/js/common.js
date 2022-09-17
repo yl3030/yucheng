ef09c76e51da006c05fd51685db42ff4e8048932
youzhen <yl301114@gmail.com>
2022-09-17 11:52:17 +0800

    選單

--- public/js/common.js
+++ public/js/common.js
@@ -9,3 +9,15 @@ $(".header_menu").click(function(){
 $(".btn-top").click(function(){
     $("html,body").animate({ scrollTop:0 },300)
 })
+
+
+// 品牌代理下拉選單
+$(".brand_listContent_kind-m-active").click(function(){
+    $(this).parents(".brand_listContent_kind-m").find(".brand_listContent_kind-m-list").slideToggle(300);
+})
+$(document).click(function (event) {
+    var area = $(".brand_listContent_kind-m");
+    if (!area.is(event.target) && area.has(event.target).length === 0) {
+        $(".brand_listContent_kind-m-list").slideUp(300);
+    }
+});
